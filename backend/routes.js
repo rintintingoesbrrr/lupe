19325bce58de003336787373516de215d82417d3
sofia-lpz <a01028251@tec.mx>
2026-01-31 18:46:32 -0600

    cruds

--- backend/routes.js
+++ backend/routes.js
@@ -5,4 +5,18 @@ const router = express.Router();
 
 router.get("/chat", controller.chatController);
 
+//crud
+
+router.post("/activity", controller.createActivityController);
+router.get("/activity/:id", controller.getActivityController);
+router.get("/activity", controller.getAllActivitiesController);
+router.put("/activity/:id", controller.updateActivityController);
+router.delete("/activity/:id", controller.deleteActivityController);
+
+//gets
+router.get("/users", controller.getUsersController);
+router.get("/coeficients", controller.getCoefficientsController);
+
+
+
 export { router }
